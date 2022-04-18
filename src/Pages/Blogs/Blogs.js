a690aaf7eb5687d1745247e70826a9ec427d6d5a
Nahid Hasan <nahidhasankiran2@gmail.com>
2022-04-18 13:51:31 +0600

    Blog added

--- src/Pages/Blogs/Blogs.js
+++ src/Pages/Blogs/Blogs.js
@@ -6,7 +6,7 @@ const Blogs = () => {
         <Container className='bg-light py-4 my-4'>
             <h2 className='text-center text-primary'>Blogs</h2>
             <article>
-                <h5>Authentication vs Authorization</h5>
+                <h4>Authentication vs Authorization</h4>
                 <p>
                     <strong>Authentication:</strong> is the process of verifying people.
                 </p>
@@ -14,14 +14,20 @@ const Blogs = () => {
                     <strong>Authorization:</strong> is the process of verifying people for specific informations like a specific username or email or something else using we can verify someone exacly we are looking for.
                 </p>
             </article>
+            <hr />
             <article>
-                <h6>Why we are using firebase? What are the alternatives?</h6>
+                <h4>Why we are using firebase? What are the alternatives?</h4>
                 <p>
                     <strong>We use firebase</strong> to interact with user informations and implement the user can access our website or not.
                 </p>
                 <p>
-                    <strong>Alternatives</strong>
+                    <strong>Alternatives:</strong> Parse, Back4App, AWS Amplify, Kuzzle, Cousebase, NativeScript.
                 </p>
+            </article>
+            <hr />
+            <article>
+                <h4>What other services firebase provide us other than authentication?</h4>
+                <p><strong>Other that authentication Firebase</strong> Provide us Cloud Firestore, Cloud Functions, Hosting, Cloud Storage, Google Analytics, Cloud Messaging. etc..</p>
             </article>
         </Container>
     );
